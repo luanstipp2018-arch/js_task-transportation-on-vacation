a3a3cb31e54d8cba78a9d84741402609b530d852
luanstipp2018-arch <luanstipp2018@gmail.com>
2026-08-03 11:28:36 -0300

    Refactor rental cost calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyPrice = 40;
-  let total = days * dailyPrice;
+  const DAILY_PRICE = 40;
+  const LONG_TERM_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DAYS = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const totalCost = days * DAILY_PRICE;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+  if (days >= LONG_TERM_DAYS) {
+    return totalCost - LONG_TERM_DISCOUNT;
   }
-  return total;
-}
 
+  if (days >= SHORT_TERM_DAYS) {
+    return totalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalCost;
+}
 
 module.exports = calculateRentalCost;
